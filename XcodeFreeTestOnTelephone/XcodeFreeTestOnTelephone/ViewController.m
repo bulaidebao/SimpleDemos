fda9165d377a9170f31373949df84ab5a815832a
bulaidebao <1738251463@qq.com>
2015-10-13 11:40:52 +0800

    This is asample for Xcode7 free Testing on equipments

---
+++ XcodeFreeTestOnTelephone/XcodeFreeTestOnTelephone/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  XcodeFreeTestOnTelephone
+//
+//  Created by apple on 15/10/13.
+//  Copyright © 2015年 facenano. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view setBackgroundColor:[UIColor redColor]];
+    
+    NSLog(@" Xcode7无证书测试程序 ");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ XcodeFreeTestOnTelephone/XcodeFreeTestOnTelephone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XcodeFreeTestOnTelephone
+//
+//  Created by apple on 15/10/13.
+//  Copyright © 2015年 facenano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
